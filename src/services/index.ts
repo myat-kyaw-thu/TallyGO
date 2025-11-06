5449b1821367a7d4f5b8587f761bb8a5e953228a
Andrew <149857208+myat-kyaw-thu@users.noreply.github.com>
2025-11-06 18:16:27 +0630

    Create index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+// Service exports for TallyGO
+export { BackgroundTaskManager, backgroundTaskManager } from './backgroundTaskManager';
+export { BackgroundTaskService, backgroundTaskService } from './backgroundTaskService';
+
+export { NotificationService, notificationService } from './notificationService';
+export { LocalStorageService } from './storage';
+export { SupabaseService, supabaseService } from './supabase';
+export { WeeklyReportService } from './weeklyReportService';
+
