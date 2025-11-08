9b9c6ba35f8e8ef57e39085b512adefa7b00db83
Andrew <149857208+myat-kyaw-thu@users.noreply.github.com>
2025-11-08 19:56:21 +0630

    Create index.ts

---
+++ src/screens/main/index.ts
@@ -0,0 +1,6 @@
+export { AccountScreen } from './AccountScreen';
+export { DataManagementScreen } from './DataManagementScreen';
+export { ExpensesScreen } from './ExpensesScreen';
+export { GoalsScreen } from './GoalsScreen';
+export { SettingsScreen } from './SettingsScreen';
+
