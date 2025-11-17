26e831ef736fe90e7fcc9030417167d2a6609bfb
Andrew <149857208+myat-kyaw-thu@users.noreply.github.com>
2025-11-17 20:44:14 +0630

    Create index.ts

---
+++ src/navigation/index.ts
@@ -0,0 +1,8 @@
+// Navigation exports
+export { AppNavigator } from './AppNavigator';
+export { AuthNavigator } from './AuthNavigator';
+export { MainTabNavigator } from './MainTabNavigator';
+export { SettingsNavigator } from './SettingsNavigator';
+
+// Re-export navigation types
+export * from '../types/navigation';
